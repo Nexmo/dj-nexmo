41dad8f4856d608cf7334bc1b340e07cbd07442a
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-05-26 13:44:27 +0100

    Minor reformat of setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(
         "console_scripts": ["dj = django.core.management:execute_from_command_line"]
     },
     install_requires=[
-        "nexmo ~= 2.0.0",
-        "django ~= 2.0.0",
-        "attrs ~= 17.4.0",
-        "marshmallow ~= 3.0.0b8",
-        "phonenumbers ~= 8.9.4",
+        "nexmo          ~= 2.0.0",
+        "django         ~= 2.0.0",
+        "attrs          ~= 17.4.0",
+        "marshmallow    ~= 3.0.0b8",
+        "phonenumbers   ~= 8.9.4",
     ],
 )
