bfe381b7566ff8592368f389cccea53e20e3b776
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-05-28 10:12:57 +0100

    Add a manifest and invoke tasks.

---
+++ tasks.py
@@ -0,0 +1,12 @@
+from invoke import task
+
+
+@task
+def build(c):
+    c.run("pandoc -f markdown -t rst -o README.rst README.md")
+    c.run("python setup.py bdist_wheel sdist")
+
+
+@task
+def clean(c):
+    c.run("rm -rf README.rst build dist")
