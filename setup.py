3ec5b6c3bb0690302327c88da6ddd50339f63a50
Mark Smith <mark.smith@practicalpoetry.co.uk>
2018-06-15 16:00:05 +0100

    Relax nexmo lib version.

--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     author_email="devrel@nexmo.com",
     url="https://developer.nexmo.com/",
     description="Utilities for Django developers using Nexmo's APIs",
+
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
+
     packages=find_packages("src"),
     package_dir={"": "src"},
     zip_safe=True,
@@ -17,7 +19,7 @@ setup(
         "console_scripts": ["dj = django.core.management:execute_from_command_line"]
     },
     install_requires=[
-        "nexmo          ~= 2.1.0",
+        "nexmo          >= 2.0.0,<3.0",
         "django         >= 2.0.0",
         "attrs          ~= 17.4.0",
         "marshmallow    ~= 3.0.0b8",
